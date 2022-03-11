290e482c9d4fdd81910bea7f70db2bf820cb5fd3
Vanim77 <vanimouzad123@gmail.com>
2022-03-11 15:37:35 -0300

    impl: Monster class

---
+++ src/Monster.ts
@@ -0,0 +1,33 @@
+import ISimpleFighter from './Fighter/SimpleFighter';
+
+class Monster implements ISimpleFighter {
+  constructor(private _lifePoints = 85, private _strength = 63) { }
+
+  public get lifePoints() {
+    return this._lifePoints;
+  }
+
+  public get strength() {
+    return this._strength;
+  }
+
+  public receiveDamage(attackPoints: number): number {
+    const damage = attackPoints;
+
+    this._lifePoints -= damage;
+
+    if (this._lifePoints <= 0) {
+      this._lifePoints = -1;
+    }
+
+    return this._lifePoints;
+  }
+
+  public attack(enemy: ISimpleFighter): void {
+    const damage = this._strength;
+
+    enemy.receiveDamage(damage);
+  }
+}
+
+export default Monster;
